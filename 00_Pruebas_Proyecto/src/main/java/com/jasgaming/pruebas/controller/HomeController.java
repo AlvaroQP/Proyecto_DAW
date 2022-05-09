98f3093e548b11adfa1dd88631dfa5518f2c8de3
aqp <alvquipom@gmail.com>
2022-05-09 21:19:05 +0200

    adaptación homecontroller

--- 00_Pruebas_Proyecto/src/main/java/com/jasgaming/pruebas/controller/HomeController.java
+++ 00_Pruebas_Proyecto/src/main/java/com/jasgaming/pruebas/controller/HomeController.java
@@ -1,34 +1,32 @@
 package com.jasgaming.pruebas.controller;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.jasgaming.pruebas.model.entities.Perfile;
+import com.jasgaming.pruebas.model.entities.Usuario;
+import com.jasgaming.pruebas.model.service.UsuarioService;
 import com.jasgaming.pruebas.model.service.VideojuegoEnConsolaService;
-import com.jasgaming.pruebas.model.service.VideojuegoYGeneroService;
+
 
 @Controller
 public class HomeController {
 
 	@Autowired
-	private VideojuegoEnConsolaService vecService;
+	private PasswordEncoder passwordEncoder;
 	
 	@Autowired
-	private VideojuegoYGeneroService vygService;
-	
-	
-	@GetMapping("/registro")
-	public String registro() {
-		return "registroUsuario";
-	}
-	
-	@PostMapping("/registro")
-	public String procesarRegistro() {
+	private VideojuegoEnConsolaService vecService;
 		
-		return "";
-	}
+	@Autowired
+	private UsuarioService usuarioService;
 	
 	@GetMapping("/index")
 	public String inicio(Model model) {
@@ -38,6 +36,36 @@ public class HomeController {
 		return "index";
 	}
 	
+	@GetMapping("/registro")
+	public String registro() {
+		return "registroUsuario";
+	}
 	
+	@PostMapping("/registro")
+	public String procesarRegistro(Usuario usuario, RedirectAttributes attr) {
+		usuario.setEnabled(1);
+		usuario.setFechaRegistro(new Date());
+		usuario.setPerfile(new Perfile(2, "cliente"));
+		usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+		
+		if(usuarioService.insertarUsuario(usuario) == 1) {
+			attr.addFlashAttribute("mensaje", "Usuario dado de alta");
+		} else {
+			attr.addFlashAttribute("mensaje", "Usuario no dado de alta");
+		}		
+		return "redirect:/registro";
+	}
+	
+	
+	// Método para generar contraseñas encriptadas de prueba
+	@GetMapping("/pwd")
+	@ResponseBody
+	public String encriptar() {
+		String encriptado = passwordEncoder.encode("jasgaming");
+		System.out.println(encriptado);
+		return encriptado;
+	}
+	
+
 	
 }
